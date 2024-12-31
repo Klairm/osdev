86a417dde6c33f40fcbdfd412500f7c35e1e5c63
Ivan Garcia <ivan@thinkpad>
2024-12-31 17:43:11 +0100

    push org notes

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
 LDFLAGS = -T link.ld -melf_i386
-OBJS = loader.o
+OBJS = loader.o kmain.o
 AS = nasm
 ASFLAGS = -f elf32
+CC = gcc
+CFLAGS = -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror
+SRC = src/*.c
 
+all: kernel.elf
 
-all: build kernel.elf
+build: loader
+	$(CC) $(CFLAGS) -c $(SRC)  -o kmain.o
 
-build: loader.s
+clean:
+	rm -rf *.o *.elf
+$(OBJS): build
+
+loader: loader.s
 	$(AS) $(ASFLAGS) loader.s
 
 kernel.elf: $(OBJS)
--- loader.s
+++ loader.s
@@ -1,32 +1,36 @@
-	global loader 		; entry symbol for the program to load
-	; constants definitions
-	MAGIC_NUMBER equ 0x1BADB002 
-	FLAGS equ 0x0
-	CHECKSUM equ -MAGIC_NUMBER
-	KERNEL_STACK_SIZE equ 4096
-	
-	align 4 		; align variables into memory locations that are divisible by 4, this is done for perfomance and something else that I should research lol
+global loader          ; Entry point for the loader
+extern sum             
+
+; Constants
+MAGIC_NUMBER      equ 0x1BADB002
+FLAGS             equ 0x0
+CHECKSUM          equ -(MAGIC_NUMBER + FLAGS)
+KERNEL_STACK_SIZE equ 4096
+
+section .multiboot
+align 4  			; align variables into memory locations that are divisible by 4, this is done for perfomance and something else that I should research lol 
 ; I think it's because how x86 stack works, the reserved space for any type is always a word ( 4 Bytes )
 	
-	section .text:
-	
+dd MAGIC_NUMBER
+dd FLAGS
+dd CHECKSUM
 
-	; variable definitions
-	dd MAGIC_NUMBER
-	dd FLAGS
-	dd CHECKSUM
-
-	section .bss:
-	mov esp, kernel_stack + KERNEL_STACK_SIZE
-	
-kernel_stack:
-	resb KERNEL_STACK_SIZE
-	
-
+section .text
+align 4
 loader:
-	mov eax, 0xBADC0FFE
-	
+    ; Set up the stack
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+
+
+    push dword 2
+    push dword 2
+    call sum
+
 .loop:
-	jmp .loop
-	
+    jmp .loop
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE  ; Reserve space for the stack
 	
